f398639cd403e9cc846b8e2cf7bfb134b450180a
NataliaColome <nmcolome@gmail.com>
2017-07-06 14:32:55 -0600

    Add authentication through oauth-github: session#create, session#destroy, root_path and github_login functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+  def create
+    if user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+    end
+    redirect_to root_path
+  end
+
+  def destroy
+    session.destroy
+    redirect_to root_path
+  end
+end
---
+++ db/migrate/20170706194554_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :nickname
+      t.string :name
+      t.string :email
+      t.string :image_url
+      t.string :token
+
+      t.timestamps
+    end
+  end
+end
