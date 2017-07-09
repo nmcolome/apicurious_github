41ff8ac85e01c6479645a079b163e6659571b174
NataliaColome <nmcolome@gmail.com>
2017-07-08 19:33:05 -0600

    add tests for user_sees_list_of_followers, user_sees_list_of_following and user_sees_their_organizations

---
+++ spec/features/user_sees_list_of_following_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature "User sees list of people they're following" do
+  before(:each) do
+    Capybara.app = ApicuriousGithub::Application
+    stub_omniauth
+  end
+
+  scenario 'as a logged in user' do
+    VCR.use_cassette("user_sees_basic_info") do
+      visit '/'
+      click_link "Sign in with Github"
+      click_link "Following"
+
+      expect(page).to have_selector('.s_avatar')
+      expect(page).to have_selector('.follow_data')
+      expect(page).to have_selector('.name_data')
+      expect(page).to have_selector('.follower_name')
+      expect(page).to have_selector('.bio')
+      expect(page).to have_selector('.contact_data')
+    end
+  end
+end
---
+++ spec/features/user_sees_their_organizations_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature "User sees the organizations they're members of" do
+  before(:each) do
+    Capybara.app = ApicuriousGithub::Application
+    stub_omniauth
+  end
+
+  scenario 'as a logged in user' do
+    VCR.use_cassette("user_sees_basic_info") do
+      visit '/'
+      click_link "Sign in with Github"
+
+      expect(page).to have_content("Organizations")
+      expect(page).to have_selector('.xs_avatar')
+    end
+  end
+end
